5ec26393212f656295a5acde5de6866799732a26
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-13 15:43:22 -0500

    Update and rename LiteDAO.sol to KaliDAO.sol
    nits on delegation / rename

--- contracts/contracts/KaliDAO.sol
+++ contracts/contracts/KaliDAO.sol
@@ -6,19 +6,19 @@ import './LiteDAOtoken.sol';
 import './LiteDAOnftHelper.sol';
 
 /// @notice Simple gas-optimized DAO core module.
-contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
+contract KaliDAO is KaliDAOtoken, KaliDAOnftHelper {
     /*///////////////////////////////////////////////////////////////
-                                  EVENTS
+                           EVENTS
     //////////////////////////////////////////////////////////////*/
 
     event NewProposal(uint256 indexed proposal);
-
+    
     event VoteCast(address indexed voter, uint256 indexed proposal, bool indexed approve);
 
     event ProposalProcessed(uint256 indexed proposal);
 
     /*///////////////////////////////////////////////////////////////
-                              DAO STORAGE
+                           DAO STORAGE
     //////////////////////////////////////////////////////////////*/
 
     uint256 public proposalCount;
@@ -34,14 +34,17 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
     mapping(uint256 => Proposal) public proposals;
 
     mapping(ProposalType => VoteType) public proposalVoteTypes;
-
-    mapping(uint256 => mapping(address => bool)) public voted;
+    
+    mapping(uint256 => mapping(address => bool)) private voted;
 
     enum ProposalType {
         MINT,
         BURN,
         CALL,
-        GOV
+        PERIOD,
+        QUORUM,
+        SUPERMAJORITY,
+        PAUSE
     }
 
     enum VoteType {
@@ -55,7 +58,7 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
         ProposalType proposalType;
         string description;
         address[] account; // member(s) being added/kicked; account(s) receiving payload
-        uint256[] amount; // value(s) to be minted/burned/spent; gov setting(s)
+        uint256[] amount; // value(s) to be minted/burned/spent; gov setting
         bytes[] payload; // data for CALL proposals
         uint256 yesVotes;
         uint256 noVotes;
@@ -63,62 +66,67 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
     }
 
     /*///////////////////////////////////////////////////////////////
-                               CONSTRUCTOR
+                           CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
     constructor(
         string memory name_,
         string memory symbol_,
         bool paused_,
-        address[] memory voters,
-        uint256[] memory shares,
+        address[] memory voters_,
+        uint256[] memory shares_,
         uint256 votingPeriod_,
         uint256 quorum_,
         uint256 supermajority_
     )
-        LiteDAOtoken(
+        KaliDAOtoken(
             name_,
             symbol_,
             paused_,
-            voters,
-            shares
+            voters_,
+            shares_
         )
-
     {
         require(votingPeriod_ <= 365 days, 'VOTING_PERIOD_MAX');
-
+        
         require(quorum_ <= 100, 'QUORUM_MAX');
-
+        
         require(supermajority_ <= 100, 'SUPERMAJORITY_MAX');
-
+        
         votingPeriod = votingPeriod_;
-
+        
         quorum = quorum_;
-
+        
         supermajority = supermajority_;
     }
 
     function setVoteTypes(
-        uint8 mint,
-        uint8 burn,
-        uint8 call,
-        uint8 gov
+        uint8 mint_,
+        uint8 burn_,
+        uint8 call_,
+        uint8 gov_
     ) external {
         require(!initialized, 'INITIALIZED');
 
-        proposalVoteTypes[ProposalType.MINT] = VoteType(mint);
+        proposalVoteTypes[ProposalType.MINT] = VoteType(mint_);
 
-        proposalVoteTypes[ProposalType.BURN] = VoteType(burn);
+        proposalVoteTypes[ProposalType.BURN] = VoteType(burn_);
 
-        proposalVoteTypes[ProposalType.CALL] = VoteType(call);
+        proposalVoteTypes[ProposalType.CALL] = VoteType(call_);
 
-        proposalVoteTypes[ProposalType.GOV] = VoteType(gov);
+        proposalVoteTypes[ProposalType.PERIOD] = VoteType(gov_);
+        
+        proposalVoteTypes[ProposalType.QUORUM] = VoteType(gov_);
+        
+        proposalVoteTypes[ProposalType.SUPERMAJORITY] = VoteType(gov_);
+        
+        proposalVoteTypes[ProposalType.PAUSE] = VoteType(gov_);
 
         initialized = true;
     }
 
     /*///////////////////////////////////////////////////////////////
-                         PROPOSAL LOGIC
+                           PROPOSAL LOGIC
     //////////////////////////////////////////////////////////////*/
 
     modifier onlyTokenHolders() {
@@ -133,16 +141,20 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
         uint256[] calldata amount,
         bytes[] calldata payload
     ) external onlyTokenHolders {
-        require(account.length == amount.length && amount.length == payload.length, "NO_ARRAY_PARITY");
-
-        require(payload.length <= 10, "ARRAY_MAX");
-
-        if (proposalType == ProposalType.GOV) {
+        require(account.length == amount.length && amount.length == payload.length, 'NO_ARRAY_PARITY');
+        
+        require(account.length <= 10, 'ARRAY_MAX');
+        
+        if (proposalType == ProposalType.PERIOD) {
             require(amount[0] <= 365 days, 'VOTING_PERIOD_MAX');
-
-            require(amount[1] <= 100, 'QUORUM_MAX');
-
-            require(amount[2] <= 100, 'SUPERMAJORITY_MAX');
+        }
+        
+        if (proposalType == ProposalType.QUORUM) {
+            require(amount[0] <= 100, 'QUORUM_MAX');
+        }
+        
+        if (proposalType == ProposalType.SUPERMAJORITY) {
+            require(amount[0] <= 100, 'SUPERMAJORITY_MAX');
         }
 
         uint256 proposal = proposalCount;
@@ -157,7 +169,7 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
             noVotes: 0,
             creationTime: block.timestamp
         });
-
+        
         // this is reasonably safe from overflow because incrementing `proposalCount` beyond
         // 'type(uint256).max' is exceedingly unlikely compared to optimization benefits
         unchecked {
@@ -169,9 +181,9 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
 
     function vote(uint256 proposal, bool approve) external onlyTokenHolders {
         require(!voted[proposal][msg.sender], 'ALREADY_VOTED');
-
+        
         Proposal storage prop = proposals[proposal];
-
+        
         // this is safe from overflow because `votingPeriod` is capped so it will not combine
         // with unix time to exceed 'type(uint256).max'
         unchecked {
@@ -179,45 +191,49 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
         }
 
         uint256 weight = getPriorVotes(msg.sender, prop.creationTime);
-
+        
         // this is safe from overflow because `yesVotes` and `noVotes` are capped by `totalSupply`
         // which is checked for overflow in `LiteDAOtoken` contract
-        unchecked {
+        unchecked { 
             if (approve) {
                 prop.yesVotes += weight;
             } else {
                 prop.noVotes += weight;
             }
         }
-
+        
         voted[proposal][msg.sender] = true;
-
+        
         emit VoteCast(msg.sender, proposal, approve);
     }
 
     function processProposal(uint256 proposal) external {
         Proposal storage prop = proposals[proposal];
-
+        
         VoteType voteType = proposalVoteTypes[prop.proposalType];
-
+        
+        // this is safe from overflow because `votingPeriod` is capped so it will not combine
+        // with unix time to exceed 'type(uint256).max'
         unchecked {
-         require(block.timestamp > prop.creationTime + votingPeriod, 'VOTING_NOT_ENDED');
+            require(block.timestamp > prop.creationTime + votingPeriod, 'VOTING_NOT_ENDED');
         }
 
         bool didProposalPass = _countVotes(voteType, prop.yesVotes, prop.noVotes);
-
+        
         // this is reasonably safe from overflow because incrementing `i` loop beyond
         // 'type(uint256).max' is exceedingly unlikely compared to optimization benefits
         if (didProposalPass) {
             unchecked {
                 if (prop.proposalType == ProposalType.MINT) {
                     for (uint256 i; i < prop.account.length; i++) {
+                        _moveDelegates(address(0), delegates(prop.account[i]), prop.amount[i]);
                         _mint(prop.account[i], prop.amount[i]);
                     }
                 }
 
                 if (prop.proposalType == ProposalType.BURN) {
                     for (uint256 i; i < prop.account.length; i++) {
+                        _moveDelegates(delegates(prop.account[i]), address(0), prop.amount[i]);
                         _burn(prop.account[i], prop.amount[i]);
                     }
                 }
@@ -227,12 +243,22 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
                         prop.account[i].call{value: prop.amount[i]}(prop.payload[i]);
                     }
                 }
-
-                if (prop.proposalType == ProposalType.GOV) {
+                
+                // governance settings
+                if (prop.proposalType == ProposalType.PERIOD) {
                     if (prop.amount[0] > 0) votingPeriod = prop.amount[0];
-                    if (prop.amount[1] > 0) quorum = prop.amount[1];
-                    if (prop.amount[2] > 0) supermajority = prop.amount[2];
-                    if (prop.amount[3] > 0) _togglePause();
+                }
+                
+                if (prop.proposalType == ProposalType.QUORUM) {
+                    if (prop.amount[0] > 0) quorum = prop.amount[0];
+                }
+                
+                if (prop.proposalType == ProposalType.SUPERMAJORITY) {
+                    if (prop.amount[0] > 0) supermajority = prop.amount[0];
+                }
+                
+                if (prop.proposalType == ProposalType.PAUSE) {
+                    if (prop.amount[0] > 0) _togglePause();
                 }
             }
         }
@@ -250,7 +276,7 @@ contract LiteDAO is LiteDAOtoken, LiteDAOnftHelper {
         // rule out any failed quorums
         if (voteType == VoteType.SIMPLE_MAJORITY_QUORUM_REQUIRED || voteType == VoteType.SUPERMAJORITY_QUORUM_REQUIRED) {
             uint256 minVotes = (totalSupply * quorum) / 100;
-
+            
             // this is safe from overflow because `yesVotes` and `noVotes` are capped by `totalSupply`
             // which is checked for overflow in `LiteDAOtoken` contract
             unchecked {
